323cc449ab25f041b303cb3efc149f3efd1d0037
seanka <seankristian16@gmail.com>
2021-10-25 12:49:40 +0700

    sealButton connected to database

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:flutter/widgets.dart';
 import 'constant.dart';
 
 class HomePage extends StatefulWidget{
@@ -15,7 +14,6 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage> {
   String _displayText = 'aaaa';
-  String _wifiStatus = '';
   String _lidStatusButton = '';
   final _database = FirebaseDatabase.instance.reference();
   final database = FirebaseDatabase.instance.reference();
@@ -24,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   late int _lid;
   late int _humid = 0;
   late int _sealStatus;
+  late String _wifiStatus = '';
 
   @override
   void initState() {
@@ -46,29 +45,33 @@ class _HomePageState extends State<HomePage> {
         _lid == 1? _lidStatusImage = 'assets/images/bin-open.png' : _lidStatusImage = 'assets/images/bin-close.png';
         _lid == 1? _lidStatusButton = 'CLOSE LID' : _lidStatusButton = 'OPEN LID';
       });
+    });
 
-      _database.child('/humidity').onValue.listen((event) {
-        _humid = event.snapshot.value;
+    _database.child('/humidity').onValue.listen((event) {
+      _humid = event.snapshot.value;
+      setState(() {
+        _humid = _humid;
       });
     });
 
     _database.child('/wifiSSID').onValue.listen((event) {
-      final String wifiSSID = event.snapshot.value;
+      _wifiStatus = event.snapshot.value;
       setState(() {
-        _wifiStatus = wifiSSID;
+        _wifiStatus = _wifiStatus;
       });
+      });
+
+    _database.child('/sealStatus').onValue.listen((event) {
+      _sealStatus = event.snapshot.value;
     });
   }
 
   void lidState() async {
-    print("button pressed!");
     if (_lid == 1) {
-      print("lid status = 1");
       try {
         await database.update({'/lidStatus': 0});
       } catch (e) {print("Exception was: $e");}
     } else {
-      print("lid status = 0");
       try {
         await database.update({'/lidStatus': 1});
       } catch (e) {print("Exception was: $e");};
@@ -76,177 +79,122 @@ class _HomePageState extends State<HomePage> {
   }
 
   void sealState() async {
-
+    if(_sealStatus == 1) {
+      try {
+        await database.update({'/sealStatus': 0});
+      } catch(e) {}
+    } else {
+      try {
+        await database.update({'/sealStatus': 1});
+      } catch (e) {}
+    }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final _screenWidth = MediaQuery.of(context).size.width;
-    final _screenHeight = MediaQuery.of(context).size.height;
+@override
+Widget build(BuildContext context) {
+  final _screenWidth = MediaQuery.of(context).size.width;
+  final _screenHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      backgroundColor: ThemeColors.mainColor,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget> [
-              Container(
-                width: _screenWidth,
-                height: _screenHeight * 0.1,
-                child: const Padding(
-                  padding: EdgeInsets.only(top: 25, left: 30, bottom: 25),
-                  child: Text(
-                    "smart bin",
-                    style: TextStyle(fontSize: 30, color: Color(0xFF807182), fontFamily: 'Sans-serif'),
-                  ),
+  return Scaffold(
+    backgroundColor: ThemeColors.mainColor,
+    body: SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: <Widget> [
+            Container(
+              width: _screenWidth,
+              height: _screenHeight * 0.1,
+              child: const Padding(
+                padding: EdgeInsets.only(top: 25, left: 30, bottom: 25),
+                child: Text(
+                  "smart bin",
+                  style: TextStyle(fontSize: 30, color: Color(0xFF807182), fontFamily: 'Sans-serif'),
                 ),
               ),
+            ),
 
-              Container(
-                  color: ThemeColors.mainColor,
-                  width: _screenWidth,
-                  height: _screenHeight * 0.5,
-                  child: Stack(
-                    children: <Widget> [
-                      Align(
-                        alignment: Alignment(0, 0.4),
-                        child: SizedBox(
-                          width: _screenWidth * 0.95,
-                          child: const Image(
-                            image: AssetImage('assets/images/background.png'),
+            Container(
+                color: ThemeColors.mainColor,
+                width: _screenWidth,
+                height: _screenHeight * 0.5,
+                child: Stack(
+                  children: <Widget> [
+                    Align(
+                      alignment: Alignment(0, 0.4),
+                      child: SizedBox(
+                        width: _screenWidth * 0.95,
+                        child: const Image(
+                          image: AssetImage('assets/images/background.png'),
+                        ),
+                      ),
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: InkWell(
+                        onTap: () {lidState();},
+                        child: Center(
+                          child: SizedBox(
+                            child: Image(image: AssetImage(_lidStatusImage),),
+                            height: _screenHeight * 0.45,
                           ),
                         ),
                       ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: InkWell(
-                          onTap: () {lidState();},
-                          child: Center(
-                            child: SizedBox(
-                              child: Image(image: AssetImage(_lidStatusImage),),
-                              height: _screenHeight * 0.45,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  )
-              ),
+                    ),
+                  ],
+                )
+            ),
 
             Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 0),
-                  child: Column(
+              padding: const EdgeInsets.symmetric(horizontal: 0),
+              child: Column(
+                children: <Widget> [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget> [
-                      Row(
+                      Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: <Widget> [
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget> [
 
-                              //WIFI STATUS
-                              Padding(
-                                padding: const EdgeInsets.only(right: 10, bottom: 10),
-                                child: Container(
-                                  width: _screenWidth * 0.45,
-                                  height: _screenHeight * 0.125,
-                                  decoration: cardDecoration,
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: <Widget> [
-                                        const Text(
-                                          "wifi status",
-                                          style: TextStyle(
-                                              fontSize: 15,
-                                              color: ThemeColors.textColor,
-                                              fontFamily: 'Comfortaa',
-                                              fontWeight: FontWeight.w300
-                                          ),
-                                        ),
-                                        Row(
-                                          crossAxisAlignment: CrossAxisAlignment.center,
-                                          children: <Widget> [
-                                            Padding(
-                                              padding: const EdgeInsets.only(top: 15, right: 15),
-                                              child: SizedBox(
-                                                height: _screenHeight * 0.025,
-                                                child: Image.asset(
-                                                    'assets/images/wifi-icon.gif'
-                                                ),
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.only(top: 15),
-                                              child: Text(
-                                                _wifiStatus,
-                                                style: descriptionText,
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ),
-
-                              //LID STATUS
-                              Padding(
-                                padding: const EdgeInsets.only(right: 10, top: 10),
-                                child: InkWell(
-                                  onTap: () { lidState(); },
-                                  child: Container(
-                                    width: _screenWidth * 0.45,
-                                    height: _screenHeight * 0.05,
-                                    decoration: cardDecoration,
-                                    child: Center(
-                                      child: Text(
-                                        _lidStatusButton,
-                                        style: descriptionText,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ],
-                          ),
-
-                          //HUMID BUTTON
+                          //WIFI STATUS
                           Padding(
-                            padding: const EdgeInsets.only(left: 10),
+                            padding: const EdgeInsets.only(right: 10, bottom: 10),
                             child: Container(
-                              width: _screenWidth * 0.4,
-                              height: _screenHeight * 0.2,
+                              width: _screenWidth * 0.45,
+                              height: _screenHeight * 0.125,
                               decoration: cardDecoration,
                               child: Padding(
-                                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
                                 child: Column(
                                   crossAxisAlignment: CrossAxisAlignment.start,
                                   children: <Widget> [
                                     const Text(
-                                      "humidity",
+                                      "wifi status",
                                       style: TextStyle(
-                                        color: ThemeColors.textColor,
-                                        fontFamily: 'Comfortaa',
-                                        fontWeight: FontWeight.w400,
+                                          fontSize: 15,
+                                          color: ThemeColors.textColor,
+                                          fontFamily: 'Comfortaa',
+                                          fontWeight: FontWeight.w300
                                       ),
                                     ),
-
                                     Row(
+                                      crossAxisAlignment: CrossAxisAlignment.center,
                                       children: <Widget> [
-                                        SizedBox(
-                                          height: _screenHeight * 0.1,
-                                          child: Image.asset(
-                                              'assets/images/humid.gif'
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 15, right: 15),
+                                          child: SizedBox(
+                                            height: _screenHeight * 0.025,
+                                            child: Image.asset(
+                                                'assets/images/wifi-icon.gif'
+                                            ),
                                           ),
                                         ),
-
-                                        Text(
-                                          _humid.toString(),
-                                          style: descriptionText,
+                                        Padding(
+                                          padding: const EdgeInsets.only(top: 15),
+                                          child: Text(
+                                            _wifiStatus,
+                                            style: descriptionText,
+                                          ),
                                         ),
                                       ],
                                     ),
@@ -255,37 +203,103 @@ class _HomePageState extends State<HomePage> {
                               ),
                             ),
                           ),
+
+                          //LID STATUS
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10, top: 10),
+                            child: InkWell(
+                              onTap: () { lidState(); },
+                              child: Container(
+                                width: _screenWidth * 0.45,
+                                height: _screenHeight * 0.05,
+                                decoration: cardDecoration,
+                                child: Center(
+                                  child: Text(
+                                    _lidStatusButton,
+                                    style: descriptionText,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
                         ],
                       ),
 
-                      //SEAL BUTTON
+                      //HUMID BUTTON
                       Padding(
-                        padding: const EdgeInsets.only(top: 20),
+                        padding: const EdgeInsets.only(left: 10),
                         child: Container(
-                          width: _screenWidth * 0.9,
-                          height: _screenHeight * 0.05,
+                          width: _screenWidth * 0.4,
+                          height: _screenHeight * 0.2,
                           decoration: cardDecoration,
-                          child: Center(
-                            child: Text(
-                              "SEAL NOW",
-                              style: descriptionText,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget> [
+                                const Text(
+                                  "humidity",
+                                  style: TextStyle(
+                                    color: ThemeColors.textColor,
+                                    fontFamily: 'Comfortaa',
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                                ),
+
+                                Row(
+                                  children: <Widget> [
+                                    SizedBox(
+                                      height: _screenHeight * 0.1,
+                                      child: Image.asset(
+                                          'assets/images/humid.gif'
+                                      ),
+                                    ),
+
+                                    Text(
+                                      _humid.toString(),
+                                      style: descriptionText,
+                                    ),
+                                  ],
+                                ),
+                              ],
                             ),
                           ),
                         ),
                       ),
                     ],
                   ),
-                ),
-            ],
-          ),
+
+                  //SEAL BUTTON
+                  Padding(
+                    padding: const EdgeInsets.only(top: 20),
+                    child: InkWell(
+                      onTap: () {sealState();},
+                      child: Container(
+                        width: _screenWidth * 0.9,
+                        height: _screenHeight * 0.05,
+                        decoration: cardDecoration,
+                        child: Center(
+                          child: Text(
+                            "SEAL NOW",
+                            style: descriptionText,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 
-  @override
-  void deactivate() {
-    _readDatabase.cancel();
-    super.deactivate();
-  }
+@override
+void deactivate() {
+  _readDatabase.cancel();
+  super.deactivate();
+}
 }
